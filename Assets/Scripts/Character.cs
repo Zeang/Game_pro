c0d192966a6e41713aa23cacea6d25e9fdb79002
Jinnm <jinnm97@gmail.com>
2019-06-09 11:22:12 +0800

    create character asset

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// 子弹类型枚举
+public enum CharacterType
+{
+    Maoxian = 0,
+    Cike,
+    Shengtu
+}
+
+/// <summary>
+/// 可序列化
+/// </summary>
+[Serializable]
+public class Character : ScriptableObject
+{
+
+    public CharacterType characType = CharacterType.Maoxian;
+
+    public GameObject fbx;
+    public GameObject weapon;
+    public AnimationClip walk_forward;
+    public AnimationClip walk_backward;
+    public AnimationClip walk_left;
+    public AnimationClip walk_right;
+    public AnimationClip walk_to_dye;
+    public AnimationClip crouch_forward;
+    public AnimationClip crouch_to_dye;
+}
