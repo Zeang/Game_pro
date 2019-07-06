3ebd5daf4f9063162de0fb6c42f08f3433372414
Florancesunlight <40712257+Florancesunlight@users.noreply.github.com>
2019-07-06 23:00:07 +0800

    Add files via upload

---
+++ Assets/Scripts/head.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class head : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onStartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Assets/Scripts/match_exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class match_exit : MonoBehaviour
+{
+ public void OnLoginButtonClick()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
